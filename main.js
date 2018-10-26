07104107335797dea82261d3e17a02721de84068
maetaro <maetaro@users.noreply.github.com>
2018-10-27 08:56:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ phina.globalize();
 // 定数
 var ASSETS = {
     //画像
-    image: {
-        bg: 'assets/image/bg.png',
-        bg2: 'assets/image/bg.png',
+    image:
         tomapiko: 'https://rawgit.com/phi-jp/phina.js/develop/assets/images/tomapiko_ss.png',
         tomato: 'assets/image/tomato.png',
         tomato_green: 'assets/image/tomato_green.png',
@@ -71,13 +69,6 @@ phina.define("MainScene", {
             .addChildTo(this.mapBase);
         this.map.tweener.clear().setUpdateType('fps');
         this.map.setScale(2, 2);
-
-        // 背景
-        this.bg = Sprite("bg").addChildTo(this.mapGroup);
-        this.bg.origin.set(0, 0); // 左上基準に変更
-        this.bg2 = Sprite("bg2").addChildTo(this.mapGroup);
-        this.bg2.origin.set(0, 0); // 左上基準に変更
-        this.bg2.setPosition(SCREEN_WIDTH, 0); // 左上基準に変更
 
         // サウンドラベル
         this.soundLabel = Label({ text: 'sound off' }).addChildTo(this);
@@ -229,13 +220,7 @@ phina.define("MainScene", {
         //    //    this.player.vx = 0;
         //    //}
         //    this.mapGroup.x -= vx;
-        //    this.mapBase.x -= vx;
-        //    //this.bg.x -= vx;
-        //    //this.bg2.x -= vx;
-        //    //if (this.bg.x <= -SCREEN_WIDTH) {
-        //    //    this.bg.x = 0;
-        //    //    this.bg2.x = SCREEN_WIDTH;
-        //    //}
+        //    this.mapBase.x -= vx;
         //    //this.blockGroup.children.each(function (block) {
         //    //    block.x -= vx;
         //    //    if (block.x < -100) {
