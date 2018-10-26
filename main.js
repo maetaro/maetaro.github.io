108f055c4c14e11c70b79a4e5acc01f7a5360b63
maetaro <maetaro@users.noreply.github.com>
2018-10-26 10:49:15 +0900

    Update main.js

--- main.js
+++ main.js
@@ -354,7 +354,7 @@ phina.define("MainScene", {
                     botom: top + unitSize,
                 };
                 collisionRect = phina.geom.Rect(left, top, unitSize, unitSize);
-                //if (!player.collider.hitTest(collisionRect)) continue;
+                if (!player.collider.hitTest(collisionRect)) continue;
                 var rect = intersect(player.collider.getAbsoluteRect(), collisionRect);
                 if (rect.height <= 0 || rect.width <= 0) {
                     continue;
