5def4da9739e9c9bf886b54eb7a073974dbb6483
maetaro <maetaro@users.noreply.github.com>
2018-11-03 07:46:46 +0900

    Update main.js

--- actionApp/main.js
+++ actionApp/main.js
@@ -253,7 +253,7 @@ phina.define("MainScene", {
                 }
 
                 var rect2 = intersect(player.collider.getAbsoluteRect(), blockRect);
-logger(rect2);
+                //logger(rect2);
 
 
                 let checkResult = player.collisionBlock(blockRect);
