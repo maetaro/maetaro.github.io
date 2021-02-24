eddf4bb40419428a12e0491327e94372aab3400d
builduser <test-builduser@sample.com>
2021-02-24 02:50:48 +0000

    auto rebuilding site

---
+++ ragbag/ragbag/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["main",""],["shuffle","Returns a shuffled collection."]]});
---
+++ ragbag/search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"ragbag":{"doc":"","i":[[5,"main","ragbag","",null,[[]]],[5,"shuffle","","Returns a shuffled collection.",null,[[["vec",3]],["vec",3]]]],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
