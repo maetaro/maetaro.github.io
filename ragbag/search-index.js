822100bbecce9c56e0fe698c1f18fde3cb4eb981
builduser <test-builduser@sample.com>
2021-02-26 07:26:33 +0000

    auto rebuilding site

--- ragbag/search-index.js
+++ ragbag/search-index.js
@@ -1,4 +1,4 @@
 var searchIndex = JSON.parse('{\
-"ragbag":{"doc":"","i":[[5,"shuffle","ragbag","Returns a shuffled collection.",null,[[["vec",3]],["vec",3]]],[5,"average","","Returns a average of collection.",null,[[["vec",3]]]],[5,"qsort","","Returns a sorted of collection. using quick sort.",null,[[],["vec",3]]],[8,"OrderExt","","",null,null],[10,"order_by","","",0,[[]]],[5,"map","","Returns a collection with f.",null,[[["vec",3]],["vec",3]]]],"p":[[8,"OrderExt"]]}\
+"ragbag":{"doc":"","i":[[5,"shuffle","ragbag","Returns a shuffled collection.",null,[[["vec",3]],["vec",3]]],[5,"average","","Returns a average of collection.",null,[[["vec",3]]]],[5,"qsort","","Returns a sorted of collection. using quick sort.",null,[[],["vec",3]]],[8,"OrderExt","","",null,null],[10,"order_by","","",0,[[]]]],"p":[[8,"OrderExt"]]}\
 }');
 addSearchOptions(searchIndex);initSearch(searchIndex);
