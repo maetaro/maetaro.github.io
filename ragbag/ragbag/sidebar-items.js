df8af0a68f5f79c1f521af255f25e7e215db7780
builduser <test-builduser@sample.com>
2021-02-26 06:59:38 +0000

    auto rebuilding site

--- ragbag/ragbag/sidebar-items.js
+++ ragbag/ragbag/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["average","Returns a average of collection."],["qsort","Returns a sorted of collection. using quick sort."],["shuffle","Returns a shuffled collection."]],"trait":[["OrderExt",""]]});
+initSidebarItems({"fn":[["average","Returns a average of collection."],["map","Returns a collection with f."],["qsort","Returns a sorted of collection. using quick sort."],["shuffle","Returns a shuffled collection."]],"trait":[["OrderExt",""]]});
